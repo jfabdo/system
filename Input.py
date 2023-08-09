aaecce9290a5c7cb757544c2fd1bba9fd75e349e
JF Abdo <jfabdo@gmail.com>
2023-08-09 17:43:04 -0500

    Added move keys

--- Input.py
+++ Input.py
@@ -1,13 +1,20 @@
 import direct.directbase.DirectStart
 from direct.showbase import DirectObject
 
-class Test(DirectObject.DirectObject):
+class Movement(DirectObject.DirectObject):
     def __init__(self):
         self.accept("Move",self._keymove)
         self.accept("Look",self._mouselook)
         self.accept("MobileMove",self._mobilemove)
+        self.registerkeys()
 
-    def _keymove(self):
+    def registerkeys(self):
+        keys = ['a','w','s','d','space','shift']
+        for i in keys:
+            self.accept(f'{i}',self._keymove,[f'{i}'])
+            self.accept(f'{i}-up',self._keymove,[f'{i}-up'])
+
+    def _keymove(self,key):
         pass #add velocity in the direction of movement
 
     def _mouselook(self):
@@ -15,3 +22,4 @@ class Test(DirectObject.DirectObject):
 
     def _mobilemove(self):
         pass #catch events from the main screen
+
