337921ea9be4f3c8de76719a680784047c7df831
JF Abdo <jfabdo@gmail.com>
2023-08-09 17:45:19 -0500

    Moved setting up panda to it's own function

--- Game.py
+++ Game.py
@@ -1,5 +1,4 @@
 from direct.showbase.ShowBase import ShowBase
-
 from direct.actor.Actor import Actor
 from panda3d.core import CollisionTraverser, CollisionHandlerPusher, CollisionSphere, CollisionTube, CollisionNode
 from panda3d.core import Spotlight,OrthographicLens,PerspectiveLens
@@ -19,17 +18,10 @@ class Game(ShowBase):
         # self.state = state
         ShowBase.__init__(self)
         self.apppath = apppath
-        self.setupPanda(size)
-        self.rooms = generaterooms()
-        self.setscene()
-        # self.setkeys()
-        self.setcollisions()
-        self.actor = Player()
-        self.players = {}
-        self.enemies = {}
-
+        self.playGame(size)
+    
     def setupPanda(self,size=None):
-        # self.disableMouse()
+        self.disableMouse()
         properties = WindowProperties()
         if size != None and size[2] > 1000:
             properties.setSize(size[2], size[3])
@@ -40,9 +32,9 @@ class Game(ShowBase):
         self.exitFunc = self.cleanup
         
     def setscene(self):
-        # lens = OrthographicLens()
-        # lens.setFilmSize(20, 15)  # Or whatever is appropriate for your scene
-        # base.cam.node().setLens(lens)
+        lens = OrthographicLens()
+        lens.setFilmSize(20, 15)
+        base.cam.node().setLens(lens)
         base.cam.setPos(0, -15, 20)
         # base.camera.setHpr(0, -70, 0)
         base.cam.lookAt(0,0,0)
@@ -52,12 +44,22 @@ class Game(ShowBase):
         lens = PerspectiveLens()
         slight.setLens(lens)
         slnp = render.attachNewNode(slight)
-        # slnp.setPos(0, -10, 10)
-        slnp.setPos(0, 15, 20)
+        slnp.setPos(0, -10, 10)
+        # slnp.setPos(0, 15, 20)
         slnp.lookAt(0, 0, 0)
 
         # slnp.lookAt(self.Player) # look at character
         render.setLight(slnp)
+
+    def playGame(self,size):
+        self.setupPanda(size)
+        self.rooms = generaterooms()
+        self.setscene()
+        # self.setkeys()
+        self.setcollisions()
+        self.players = {}
+        self.players['main'] = Player()
+        self.enemies = {}
     
     def setcollisions(self):
         pass
